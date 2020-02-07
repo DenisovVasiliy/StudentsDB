a7f2438f18e729ef284c0815f582e42bbd616777
VasyaDen <vasyaden@mail.ru>
2020-02-07 11:46:37 +0300

    Equals/Hashcode was overrided. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/studentsDB/domain/Student.java
+++ src/main/java/com/foxminded/studentsDB/domain/Student.java
@@ -10,11 +10,6 @@ public class Student {
     private String lastName;
     private int groupID;
 
-    public Student (String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
     public Student (int id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
@@ -48,5 +43,20 @@ public class Student {
     public boolean assignToCourse(Course course) throws DAOException {
         StudentDAO studentDAO = new StudentDAO();
         return studentDAO.assignToCourse(this, course);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+
+        Student student = (Student) o;
+
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
     }
 }

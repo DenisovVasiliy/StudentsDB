6c242e9eab12d0b86484fe6a6711dace0b317b91
VasyaDen <vasyaden@mail.ru>
2020-02-07 13:15:42 +0300

    Cleaning. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/studentsDB/domain/Main.java
+++ src/main/java/com/foxminded/studentsDB/domain/Main.java
@@ -4,14 +4,14 @@ import com.foxminded.studentsDB.dao.DAOException;
 import com.foxminded.studentsDB.dao.QueryConstants;
 import com.foxminded.studentsDB.dao.ScriptExecutor;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) throws DAOException {
         ScriptExecutor scriptExecutor = new ScriptExecutor();
 
         scriptExecutor.executeScript(QueryConstants.CREATE_TABLES);
+
         DataCreator dataCreator = new DataCreator();
         TestData testData = dataCreator.createTestData();
+
     }
 }

108da8b723fa3664cc1a6fc73027653516a5e410
VasyaDen <vasyaden@mail.ru>
2020-02-07 11:47:46 +0300

    Resource's constants was added. Signed-off-by: VasyaDen <vasyaden@mail.ru>

---
+++ src/main/java/com/foxminded/studentsDB/dao/DataConstants.java
@@ -0,0 +1,7 @@
+package com.foxminded.studentsDB.dao;
+
+public class DataConstants {
+    public static String FIRST_NAMES = "firstNames.txt";
+    public static String LAST_NAMES = "lastNames.txt";
+    public static String COURSES = "courses.txt";
+}

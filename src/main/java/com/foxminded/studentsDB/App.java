a0a65aee82fb66db5f69e62a53aecfe291b74477
VasyaDen <vasyaden@mail.ru>
2020-02-18 22:42:23 +0300

    Some cleanings. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/studentsDB/App.java
+++ src/main/java/com/foxminded/studentsDB/App.java
@@ -1,12 +1,13 @@
-package com.foxminded.studentsDB.domain;
+package com.foxminded.studentsDB;
 
 import com.foxminded.studentsDB.dao.DAOException;
 import com.foxminded.studentsDB.dao.QueryConstants;
 import com.foxminded.studentsDB.dao.ScriptExecutor;
+import com.foxminded.studentsDB.domain.DataCreator;
 import com.foxminded.studentsDB.ui.ConsoleListener;
 import com.foxminded.studentsDB.ui.Listener;
 
-public class Main {
+public class App {
     public static void main(String[] args) throws DAOException {
         ScriptExecutor scriptExecutor = new ScriptExecutor();
 
--- src/main/java/com/foxminded/studentsDB/dao/QueryConstants.java
+++ src/main/java/com/foxminded/studentsDB/dao/QueryConstants.java
@@ -7,7 +7,7 @@ public class QueryConstants {
     public static final String GET_STUDENT_BY_COURSE_FILE = "getStudentsByCourseName.sql";
     public static final String DELETE_STUDENT_FILE = "deleteStudent.sql";
     public static final String ASSIGN_TO_COURSE_FILE = "assignToCourse.sql";
-    public static final String GET_ASSIGNMENTS_FILE = "getAssignments.sql";
+    public static final String GET_ASSIGNMENTS_FILE = "getAssignmentsByStudentID.sql";
     public static final String DELETE_FROM_COURSE_FILE = "deleteFromCourse.sql";
     public static final String GET_GROUPS_BY_COUNTER = "getGroupsByCounter.sql";
     public static final String INSERT_GROUPS = "insertGroups.sql";

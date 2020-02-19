65bdbd7223b4693f38de7e3cf595d2c3fb81534a
VasyaDen <vasyaden@mail.ru>
2020-02-19 23:43:16 +0300

    Cleaning Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/studentsDB/dao/DataConstants.java
+++ src/main/java/com/foxminded/studentsDB/dao/DataConstants.java
@@ -1,7 +1,7 @@
 package com.foxminded.studentsDB.dao;
 
 public class DataConstants {
-    public static String FIRST_NAMES = "firstNames.txt";
-    public static String LAST_NAMES = "lastNames.txt";
-    public static String COURSES = "courses.txt";
+    public static String FIRST_NAMES = "FirstNames.txt";
+    public static String LAST_NAMES = "LastNames.txt";
+    public static String COURSES = "Courses.txt";
 }

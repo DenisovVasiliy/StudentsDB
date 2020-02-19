d6be6942d70b20ee09b351de432668e5be379c44
VasyaDen <vasyaden@mail.ru>
2020-02-19 13:03:05 +0300

    Resource names changed to start with capital letter. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/main/java/com/foxminded/studentsDB/dao/QueryConstants.java
+++ src/main/java/com/foxminded/studentsDB/dao/QueryConstants.java
@@ -1,18 +1,18 @@
 package com.foxminded.studentsDB.dao;
 
 public class QueryConstants {
-    public static final String INSERT_STUDENT_FILE = "insertStudents.sql";
-    public static final String INSERT_TO_GROUP_FILE = "insertStudentsToGroup.sql";
-    public static final String GET_STUDENTS_FILE = "getAllStudents.sql";
-    public static final String GET_STUDENT_BY_COURSE_FILE = "getStudentsByCourseName.sql";
-    public static final String DELETE_STUDENT_FILE = "deleteStudent.sql";
-    public static final String ASSIGN_TO_COURSE_FILE = "assignToCourse.sql";
-    public static final String GET_ASSIGNMENTS_FILE = "getAssignmentsByStudentID.sql";
-    public static final String DELETE_FROM_COURSE_FILE = "deleteFromCourse.sql";
-    public static final String GET_GROUPS_BY_COUNTER = "getGroupsByCounter.sql";
-    public static final String INSERT_GROUPS = "insertGroups.sql";
-    public static final String GET_ALL_COURSES = "getAllCourses.sql";
-    public static final String INSERT_COURSES = "insertCourses.sql";
+    public static final String INSERT_STUDENT_FILE = "InsertStudents.sql";
+    public static final String INSERT_TO_GROUP_FILE = "InsertStudentsToGroup.sql";
+    public static final String GET_STUDENTS_FILE = "GetAllStudents.sql";
+    public static final String GET_STUDENT_BY_COURSE_FILE = "GetStudentsByCourseName.sql";
+    public static final String DELETE_STUDENT_FILE = "DeleteStudent.sql";
+    public static final String ASSIGN_TO_COURSE_FILE = "AssignToCourse.sql";
+    public static final String GET_ASSIGNMENTS_FILE = "GetAssignmentsByStudentID.sql";
+    public static final String DELETE_FROM_COURSE_FILE = "DeleteFromCourse.sql";
+    public static final String GET_GROUPS_BY_COUNTER = "GetGroupsByCounter.sql";
+    public static final String INSERT_GROUPS = "InsertGroups.sql";
+    public static final String GET_ALL_COURSES = "GetAllCourses.sql";
+    public static final String INSERT_COURSES = "InsertCourses.sql";
     public static final String CREATE_TABLES = "CreateTables.sql";
-    public static final String GET_STUDENT_BY_ID = "getStudentById.sql";
+    public static final String GET_STUDENT_BY_ID = "GetStudentById.sql";
 }

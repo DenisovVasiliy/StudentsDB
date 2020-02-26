1bb2302b807cab0a84ea9e10cbb90117fc44e179
VasyaDen <vasyaden@mail.ru>
2020-02-26 13:26:00 +0300

    DAOFactory tests. Signed-off-by: VasyaDen <vasyaden@mail.ru>

---
+++ src/test/java/com/foxminded/studentsDB/dao/infra/DAOFactoryTest.java
@@ -0,0 +1,31 @@
+package com.foxminded.studentsDB.dao.infra;
+
+import com.foxminded.studentsDB.dao.exceptions.DAOException;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DAOFactoryTest {
+
+
+    @Test
+    void getConnection() throws DAOException, SQLException {
+        DAOFactory daoFactory = DAOFactory.getInstance();
+        try(Connection connection = daoFactory.getConnection()) {
+            assertTrue(connection.isValid(1));
+            assertFalse(connection.isClosed());
+        }
+    }
+
+    @Test
+    void getTestConnection() throws DAOException, SQLException {
+        DAOFactory daoFactory = DAOFactory.getInstance("TestDatabase.properties");
+        try(Connection connection = daoFactory.getConnection()) {
+            assertTrue(connection.isValid(1));
+            assertFalse(connection.isClosed());
+        }
+    }
+}

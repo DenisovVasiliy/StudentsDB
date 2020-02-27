0abb79bea0fdab72fe6e4741b79aeca3c0bbd229
VasyaDen <vasyaden@mail.ru>
2020-02-27 15:39:39 +0300

    CourseDAO tests added. Signed-off-by: VasyaDen <vasyaden@mail.ru>

---
+++ src/test/java/com/foxminded/studentsDB/dao/CourseDAOTest.java
@@ -0,0 +1,101 @@
+package com.foxminded.studentsDB.dao;
+
+import com.foxminded.studentsDB.dao.exceptions.DAOException;
+import com.foxminded.studentsDB.dao.exceptions.MessagesConstantsDAO;
+import com.foxminded.studentsDB.dao.infra.DAOFactory;
+import com.foxminded.studentsDB.dao.infra.DataReader;
+import com.foxminded.studentsDB.dao.infra.ScriptExecutor;
+import com.foxminded.studentsDB.domain.Course;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CourseDAOTest {
+    private Course course = new Course("Test Course", "Course for testing.");
+    private List<Course> testCourseList = Collections.singletonList(course);
+    private DataReader dataReader = DataReader.getInstance();
+    private static DAOFactory daoFactory;
+
+
+    @BeforeAll
+    public static void prepare() throws DAOException {
+        daoFactory = DAOFactory.getInstance("TestDatabaseH2.properties");
+    }
+
+    @BeforeEach
+    public void createTables() throws DAOException {
+        ScriptExecutor scriptExecutor = new ScriptExecutor();
+        scriptExecutor.executeScript(QueryConstants.CREATE_TABLES);
+    }
+
+    @Test
+    public void shouldInsertCoursesIntoDB() throws DAOException {
+        CourseDAO courseDAO = new CourseDAO();
+        List<Course> courses = courseDAO.getAllCourses();
+        assertEquals(0, courses.size());
+
+        courseDAO.insertCourses(testCourseList);
+
+        String script = dataReader.getQuery(QueryConstants.GET_ALL_COURSES);
+        List<Course> actualCourses = null;
+        try (Connection connection = daoFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(script);
+             ResultSet resultSet = statement.executeQuery()) {
+            actualCourses = processCoursesSet(resultSet);
+        } catch (SQLException e) {
+            throw new DAOException(MessagesConstantsDAO.CANNOT_GET_COURSES, e);
+        }
+
+        assertEquals(testCourseList, actualCourses);
+    }
+
+    @Test
+    public void shouldGetCoursesFromDB() throws DAOException {
+        CourseDAO courseDAO = new CourseDAO();
+        String script = dataReader.getQuery(QueryConstants.INSERT_COURSES);
+        try (Connection connection = daoFactory.getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement(script, Statement.RETURN_GENERATED_KEYS)) {
+            for (Course course : testCourseList) {
+                statement.setString(1, course.getName());
+                statement.setString(2, course.getDescription());
+                statement.addBatch();
+            }
+            statement.executeBatch();
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                for(Course course : testCourseList) {
+                    if(resultSet.next()) {
+                        course.setId(resultSet.getInt(1));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DAOException(MessagesConstantsDAO.CANNOT_INSERT_COURSES, e);
+        }
+
+        List<Course> actualCourses = courseDAO.getAllCourses();
+        assertEquals(testCourseList, actualCourses);
+    }
+
+    private List<Course> processCoursesSet(ResultSet resultSet) throws DAOException {
+        List<Course> courses = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                Course course = new Course(resultSet.getInt("id"), resultSet.getString("name"),
+                        resultSet.getString("description"));
+                courses.add(course);
+            }
+        } catch (SQLException e) {
+            throw new DAOException(MessagesConstantsDAO.CANNOT_PROCESS_COURSES_SET, e);
+        }
+        return courses;
+    }
+}

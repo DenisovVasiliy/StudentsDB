c063f6a1f45f55330f722700cd1ab413be0ef568
VasyaDen <vasyaden@mail.ru>
2020-03-04 14:23:50 +0300

    Tests updated. Signed-off-by: VasyaDen <vasyaden@mail.ru>

--- src/test/java/com/foxminded/studentsDB/dao/CourseDAOTest.java
+++ src/test/java/com/foxminded/studentsDB/dao/CourseDAOTest.java
@@ -4,24 +4,28 @@ import com.foxminded.studentsDB.dao.exceptions.DAOException;
 import com.foxminded.studentsDB.dao.infra.DAOFactory;
 import com.foxminded.studentsDB.dao.infra.ScriptExecutor;
 import com.foxminded.studentsDB.domain.Course;
+import com.foxminded.studentsDB.domain.Student;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CourseDAOTest {
-    private Course course = new Course("Test Course", "Course for testing.");
-    private List<Course> testCourseList = Collections.singletonList(course);
+    private static List<Course> testCourseList = new ArrayList<>();
     private static DAOFactory daoFactory;
 
 
     @BeforeAll
     public static void prepare() throws DAOException {
         daoFactory = DAOFactory.getInstance("TestDatabaseH2.properties");
+        for (int i = 0; i < 2; i++) {
+            testCourseList.add(new Course("TestCourse-" + (i + 1), "Course for testing."));
+        }
     }
 
     @BeforeEach
@@ -49,5 +53,13 @@ class CourseDAOTest {
         courseDAO.insertCourses(testCourseList);
         List<Course> actualCourses = courseDAO.getAllCourses();
         assertEquals(testCourseList, actualCourses);
+    }
+
+    @Test
+    public void shouldGetCourseById() throws DAOException {
+        CourseDAO courseDAO = new CourseDAO();
+        courseDAO.insertCourses(testCourseList);
+        Course actualCourse = courseDAO.getCourseById(testCourseList.get(1).getId());
+        assertEquals(testCourseList.get(1), actualCourse);
     }
 }
